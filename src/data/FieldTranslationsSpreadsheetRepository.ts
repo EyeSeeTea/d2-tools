917a7b4d4da8e938baae2d51ea0207f12c294c48
Arnau Sanchez <pyarnau@gmail.com>
2022-06-20 13:13:56 +0200

    Refactor FieldTranslationsSpreadsheetRepository

--- src/data/FieldTranslationsSpreadsheetRepository.ts
+++ src/data/FieldTranslationsSpreadsheetRepository.ts
@@ -9,85 +9,93 @@ import {
 } from "domain/repositories/FieldTranslationsRepository";
 import { SpreadsheetXlsxDataSource } from "domain/repositories/SpreadsheetXlsxRepository";
 import log from "utils/log";
+import { Sheet } from "./SpreadsheetsDataSource";
 
 export class FieldTranslationsSpreadsheetRepository implements FieldTranslationsRepository {
     async get<Field extends string>(
         options: GetFieldTranslationsOptions<Field>
     ): Async<FieldTranslations<Field>[]> {
-        const { translatableField, inputFile, skipHeaders, countryMapping } = options;
-
-        const spreadsheet = await new SpreadsheetXlsxDataSource().read({
-            inputFile,
-            skipHidden: false,
-        });
+        const { translatableField, inputFile, skipHeaders } = options;
+        const spreadsheet = await new SpreadsheetXlsxDataSource().read({ inputFile, skipHidden: false });
 
         const fieldTranslations = _(spreadsheet.sheets)
-            .flatMap(sheet => {
+            .flatMap((sheet): Array<FieldTranslations<Field>> => {
                 // Get headers from first row and create the locale mapping.
                 const referenceRow = _.first(sheet.rows);
                 const headers = _.keys(referenceRow);
                 const locales = _.difference(headers, [identifierField, translatableField, ...skipHeaders]);
                 if (!referenceRow) return [];
 
-                const localeMapping: Record<string, LocaleIso839_1> = _(locales)
-                    .map(locale => {
-                        const isoCode = isoLanguageCodesByName[locale];
-
-                        if (!isoCode) {
-                            log.warn(`Unknown locale name: ${locale}`);
-                        } else {
-                            return [locale, isoCode.iso639_1] as [string, LocaleIso839_1];
-                        }
-                    })
-                    .compact()
-                    .fromPairs()
-                    .value();
-
-                return sheet.rows.map((row, idx) => {
-                    const headers = _.keys(row);
-                    const locales = _.difference(headers, [
-                        identifierField,
-                        translatableField,
-                        ...skipHeaders,
-                    ]);
-                    const identifier = row[identifierField];
-                    const fieldValue = row[translatableField];
-                    const rowStr = JSON.stringify(row);
-                    const notFoundMsg = `not found in ${sheet.name}:${idx}: ${rowStr}`;
-
-                    if (!identifier) {
-                        log.warn(`Identifier field (${identifierField}) ${notFoundMsg}`);
-                        return;
-                    } else if (!fieldValue) {
-                        log.warn(`Field ${translatableField} ${notFoundMsg}`);
-                        return;
-                    }
-
-                    const translations: FieldTranslations<Field>["translations"] = _(locales)
-                        .map(locale => {
-                            const localeCode = localeMapping[locale];
-                            const value = row[locale];
-                            const countryCode = localeCode ? countryMapping[localeCode] : undefined;
-                            return localeCode && value
-                                ? { locale: _([localeCode, countryCode]).compact().join("_"), value }
-                                : undefined;
-                        })
-                        .compact()
-                        .value();
-
-                    const fieldTranslation: FieldTranslations<Field> = {
-                        identifier: identifier,
-                        field: translatableField,
-                        value: fieldValue,
-                        translations,
-                    };
-
-                    return fieldTranslation;
-                });
+                const localeMapping: Record<string, LocaleIso839_1> = this.getLocaleMapping(locales);
+                return this.getFieldTranslationsFromSheet<Field>(sheet, localeMapping, options);
             })
             .value();
 
         return _.compact(fieldTranslations);
+    }
+
+    private getFieldTranslationsFromSheet<Field extends string>(
+        sheet: Sheet<string>,
+        localeMapping: Record<string, LocaleIso839_1>,
+        options: GetFieldTranslationsOptions<Field>
+    ): Array<FieldTranslations<Field>> {
+        const { translatableField, skipHeaders, countryMapping } = options;
+
+        const translations = sheet.rows.map((row, idx) => {
+            const headers = _.keys(row);
+            const locales = _.difference(headers, [identifierField, translatableField, ...skipHeaders]);
+            const identifier = row[identifierField];
+            const fieldValue = row[translatableField];
+            const rowStr = JSON.stringify(row);
+            const notFoundMsg = `not found in ${sheet.name}:${idx}: ${rowStr}`;
+
+            if (!identifier) {
+                log.warn(`Identifier field (${identifierField}) ${notFoundMsg}`);
+                return;
+            } else if (!fieldValue) {
+                log.warn(`Field ${translatableField} ${notFoundMsg}`);
+                return;
+            }
+
+            const translations: FieldTranslations<Field>["translations"] = _(locales)
+                .map(locale => {
+                    const localeCode = localeMapping[locale];
+                    const value = row[locale];
+                    const countryCode = localeCode ? countryMapping[localeCode] : undefined;
+                    return localeCode && value
+                        ? { locale: _([localeCode, countryCode]).compact().join("_"), value }
+                        : undefined;
+                })
+                .compact()
+                .value();
+
+            const fieldTranslation: FieldTranslations<Field> = {
+                identifier: identifier,
+                field: translatableField,
+                value: fieldValue,
+                translations,
+            };
+
+            return fieldTranslation;
+        });
+
+        return _.compact(translations);
+    }
+
+    private getLocaleMapping(locales: string[]): Record<string, string> {
+        return _(locales)
+            .map(locale => {
+                const isoCode = isoLanguageCodesByName[locale];
+
+                if (!isoCode) {
+                    log.warn(`Unknown locale name: ${locale}`);
+                } else {
+                    return [locale, isoCode.iso639_1] as [string, LocaleIso839_1];
+                }
+            })
+            .compact()
+            .fromPairs()
+            .value();
     }
 }
 
--- src/domain/repositories/SpreadsheetXlsxRepository.ts
+++ src/domain/repositories/SpreadsheetXlsxRepository.ts
@@ -7,13 +7,13 @@ import { ReadOptions, Row, Sheet, Spreadsheet, SpreadsheetDataSource } from "dat
 export class SpreadsheetXlsxDataSource implements SpreadsheetDataSource {
     async read(options: ReadOptions): Async<Spreadsheet> {
         const { inputFile, skipHidden } = options;
-
         const workbook = XLSX.readFile(inputFile);
 
         const sheets = _(workbook.Sheets)
             .toPairs()
             .map(([sheetName, worksheet]): Sheet => {
                 const rows = XLSX.utils.sheet_to_json<Row<string>>(worksheet, { raw: true, skipHidden });
+
                 return {
                     name: sheetName,
                     rows: rows.map(row => _.mapValues(row, cellValue => cellValue.trim())),
