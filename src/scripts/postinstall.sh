4f74bc3f1a3c622708971809f2c511c0c3115c20
Arnau Sanchez <pyarnau@gmail.com>
2022-07-12 11:48:15 +0200

    Refactor postinstall script

--- src/scripts/postinstall.sh
+++ src/scripts/postinstall.sh
@@ -2,17 +2,17 @@
 set -e -u -o pipefail
 
 project_url="https://github.com/dhis2/capture-app"
-tag="v100.5.6"
-echo "Get capture-app: $project_url (tag $tag)"
-svn export -q --force "$project_url/tags/$tag/src/core_modules" src/data/capture-core-modules
-
-echo "Create links so absolute imports in capture-app work"
-ln -sf ./data/capture-core-modules/capture-core/ src
-ln -sf ./data/capture-core-modules/capture-core-utils/ src
+ref="tags/v100.5.6"
+echo "Get capture-app: $project_url ($ref)"
+svn export -q --force "$project_url/$ref/src/core_modules"
 
 echo "Clean flow types"
-npx flow-remove-types -q --out-dir src/data/capture-core-modules src/data/capture-core-modules
-rm -rf src/data/capture-core-modules/capture-core/flow
-find src/data/capture-core-modules -type f -print0 | xargs -0 awk -i inplace '!/^export type/'
+npx flow-remove-types -q --out-dir core_modules core_modules
+rm -rf core_modules/capture-core/flow
+find core_modules/ -type f -print0 | xargs -0 awk -i inplace '!/^export type/'
 awk -i inplace '!/^\s*(converterObject|processValue)\s*;?\s*$/' \
-    src/capture-core-utils/rulesEngine/processors/ValueProcessor.js
+    core_modules/capture-core-utils/rulesEngine/processors/ValueProcessor.js
+
+echo "Move folders"
+mv core_modules/capture-core src
+mv core_modules/capture-core-utils src
