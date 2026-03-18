1fc356dbfb6d953004c62275da0eb7d8ca10481a
idelcano <ignaciodelcano@gmail.com>
2026-03-18 19:48:54 +0100

    added missing method in repo interface

--- src/domain/repositories/user-monitoring/two-factor-monitoring/TwoFactorUserRepository.ts
+++ src/domain/repositories/user-monitoring/two-factor-monitoring/TwoFactorUserRepository.ts
@@ -1,6 +1,8 @@
 import { Async } from "domain/entities/Async";
+import { DisableUserResult } from "domain/entities/user-monitoring/two-factor-monitoring/DisableUsersResult";
 import { TwoFactorUser } from "domain/entities/user-monitoring/two-factor-monitoring/TwoFactorUser";
 
 export interface TwoFactorUserRepository {
     getUsersNotInGroupIds(excludeUserGroupIds: string[]): Async<TwoFactorUser[]>;
+    disableUsers(userIds: string[]): Async<DisableUserResult[]>;
 }
