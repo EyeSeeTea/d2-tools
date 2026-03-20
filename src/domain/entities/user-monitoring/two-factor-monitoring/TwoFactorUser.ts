dae46e4c38cb3e34511ba78ebe9b8fb8014510ac
idelcano <ignaciodelcano@gmail.com>
2026-03-20 13:21:42 +0100

    added filter logic inside twofactor user domain entity

--- src/domain/entities/user-monitoring/two-factor-monitoring/TwoFactorUser.ts
+++ src/domain/entities/user-monitoring/two-factor-monitoring/TwoFactorUser.ts
@@ -1,4 +1,5 @@
 import { Id, NamedRef } from "domain/entities/Base";
+import { Maybe } from "utils/ts-utils";
 
 export interface TwoFactorUser {
     id: Id;
@@ -7,4 +8,26 @@ export interface TwoFactorUser {
     disabled: boolean;
     externalAuth: boolean;
     userGroups: NamedRef[];
+    created: Maybe<string>;
+}
+
+export function filterByCreationDate(
+    users: TwoFactorUser[],
+    disableAfterMonths: number | undefined
+): TwoFactorUser[] {
+    if (disableAfterMonths === undefined) {
+        throw new Error(
+            "filteredByMonth is enabled but disableAfterMonths is not configured in the datastore."
+        );
+    }
+
+    const now = new Date();
+    return users.filter(user => {
+        if (!user.created) return false;
+        const createdDate = new Date(user.created);
+        const monthsDiff =
+            (now.getFullYear() - createdDate.getFullYear()) * 12 +
+            (now.getMonth() - createdDate.getMonth());
+        return monthsDiff >= disableAfterMonths;
+    });
 }
